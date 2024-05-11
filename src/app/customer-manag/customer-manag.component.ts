ab6856b98cc8b87258beeb77c6ba9c92ac6396b8
DESKTOP-3UK9LGJ\Hp <booshna012@gmail.com>
2024-05-11 08:16:39 +0530

    customer management fixed

--- src/app/customer-manag/customer-manag.component.ts
+++ src/app/customer-manag/customer-manag.component.ts
@@ -2,94 +2,95 @@ import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
-
 @Component({
   selector: 'app-customer-manag',
   templateUrl: './customer-manag.component.html',
-  styleUrls: ['./customer-manag.component.css']
+  styleUrls: ['./customer-manag.component.css'],
 })
 export class CustomerManagComponent implements OnInit {
-  constructor(private _http:HttpClient) { }
+  dataTable: any = [];
+  id: any;
+  constructor(private _http: HttpClient) {}
   ngOnInit(): void {
-    this.dataget()
+    this.dataget();
   }
 
-  private auth ='http://localhost:3000/customerManagement'
-  datatable = [
-    {
-      id: "1",
-      firstName: "Kunwar",
-      LastName: "Bahadur kashyap",
-      custContact: 987465468,
-      altcontact: 8865080562,
-      Wallet: 174,
-      custBox:215,
-      custAddres: "Nagla Ajeeta",
-      custlandMark: "mandir",
-      custCity: "Agra",
-    }
-  ];
+  private auth = 'http://localhost:3000/customerManagement';
 
   btndataView = true;
-  AlertView=false;
-  @ViewChild ('datalogin', {read: NgForm}) FormEditTemp!:NgForm;
-
+  AlertView = false;
+  @ViewChild('datalogin', { read: NgForm }) FormEditTemp!: NgForm;
 
   // POST Data
-  dataSend(formData:any){
-    if(this.btndataView){
+  dataSend(formData: any) {
+    if (this.btndataView) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
-      this._http.post(this.auth,formData).subscribe(res=>{
+      console.log(formData, 'FORMs');
+
+      this._http.post(this.auth, formData).subscribe((res) => {
         console.log(res);
         this.AlertView = true;
-        this.FormEditTemp.resetForm();  
-      })
-    }else{   // Update Operation Fill-2
-      if(confirm('Are you Sure want to update this entry')){
-        this._http.put(this.auth,formData).subscribe(res=>{
-          console.log(res);
-        })
+        this.FormEditTemp.resetForm();
+      });
+    } else {
+      // Update Operation Fill-2
+      if (confirm('Are you Sure want to update this entry')) {
+        this._http.put(`${this.auth}/${this.id}`, formData).subscribe((res) => {
+          this.AlertView = true;
+          this.FormEditTemp.resetForm();
+        });
       }
     }
+
+    this.dataget();
   }
 
   // Delete Data -- DONE
-  del(i:any){
-    if(confirm("Do You want to delete this : "+this.datatable[i].firstName+" "+this.datatable[i].LastName)){
-      this.datatable.splice(i);
-      this._http.delete(`${this.auth}/${i}`).subscribe(res=>{
+  del(id: any, i: any) {
+    if (
+      confirm(
+        'Do You want to delete this : ' +
+          this.dataTable[i].firstName +
+          ' ' +
+          this.dataTable[i].LastName
+      )
+    ) {
+      console.log(i, 'index');
+      const index = i + 1;
+      console.log(`${this.auth}/${id}`);
+
+      this._http.delete(`${this.auth}/${id}`).subscribe((res) => {
         console.log(res);
-      })
+        this.dataget();
+      });
     }
   }
 
   // Fatch Data   -- DONE
-  dataget(){
-    this._http.get(this.auth).subscribe(res=>{
-       const dataupdate = JSON.stringify(res);
-       this.datatable = JSON.parse(dataupdate);
-       console.log(res);
-    })
+  dataget() {
+    this._http.get(this.auth).subscribe((res) => {
+      this.dataTable = res;
+      // this.datatable = JSON.parse(dataupdate);
+      console.log(res);
+    });
   }
 
-  
   // Update Operation Fill-1 - DONE
-  edit(i:any){
+  edit(i: any, id: any) {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     this.btndataView = false;
+    this.id = id;
     let sr = i;
     this.FormEditTemp.setValue({
-      firstName: this.datatable[i].firstName,
-      LastName: this.datatable[i].LastName,
-      custContact: this.datatable[i].custContact,
-      altcontact: this.datatable[i].altcontact,
-      Wallet: this.datatable[i].Wallet,
-      custBox: this.datatable[i].custBox,
-      custAddres: this.datatable[i].custAddres,
-      custlandMark: this.datatable[i].custlandMark,
-      custCity: this.datatable[i].custCity,
-    })
+      firstName: this.dataTable[i].firstName,
+      LastName: this.dataTable[i].LastName,
+      custContact: this.dataTable[i].custContact,
+      altcontact: this.dataTable[i].altcontact,
+      Wallet: this.dataTable[i].Wallet,
+      custBox: this.dataTable[i].custBox,
+      custAddres: this.dataTable[i].custAddres,
+      custlandMark: this.dataTable[i].custlandMark,
+      custCity: this.dataTable[i].custCity,
+    });
   }
-
-
 }
